d32dd4228985d2a42b5e9d2a4853442d1f751740
Rasul <magomedrasul.magomedov@vitech.team>
2024-06-10 10:38:18 +0500

    refactoring

--- internal/build/http.go
+++ internal/build/http.go
@@ -35,7 +35,7 @@ func (b *Builder) ConfigureAPI(ctx context.Context) func(ctx context.Context) er
 					Error().
 					Err(err).
 					Str("path", c.Path()).
-					Msg("error configuring api")
+					Msg("handling request")
 			}
 
 			_ = c.Status(fiber.StatusInternalServerError).JSON(rest.Error{
